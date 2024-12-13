500b8c754ac1dbf6df40d40b42fae2f9f427cd37
Sara Mohamed <sara.sae1212@gmail.com>
2024-12-13 20:52:58 +0200

    t3

--- Lab 2 - Vector Addition using parallel for.cpp
+++ Lab 2 - Vector Addition using parallel for.cpp
@@ -33,3 +33,40 @@ int main(int argc, char* argv[])
 
 }
 
+
+// #include<omp.h>
+// #include<iostream>
+// #define N 10
+// using namespace std;
+// int main() {
+
+// 	omp_set_num_threads(5);
+// 	int id,nthread;
+// 	int a[N], b[N], c[N];
+// 	int i;
+// 	for (i = 0; i < N; i++) {
+// 		a[i] = b[i] = 1;
+// 	}
+
+	
+// //#pragma omp parallel private(id,i) 
+// #pragma omp parallel shared(a, b, c, nthread) private(i,id)
+// 	{
+// 		nthread = omp_get_num_threads();
+// 		id = omp_get_thread_num();
+// 		if (id == 0) {
+// 			printf(" nthread %d\n", nthread);
+// 		}
+
+		
+// 	#pragma omp for 
+// 		//for (i = id * (N / nthread); i < (id + 1) * (N / nthread); i++) {
+// 		for (i = 0; i < N; i++) {
+// 			c[i] = a[i] + b[i];
+// 			printf("id %d ----  i:%d,  c: %d \n",id,i, c[i]);
+// 		}
+// 	}
+// 	return 0;
+// }
+
+
