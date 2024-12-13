e8b19b27a003af7fe7260c668151eaa19da1abcc
Sara Mohamed <sara.sae1212@gmail.com>
2024-12-13 23:31:33 +0200

    t9

--- Lab 4 - Critical Directive.cpp
+++ Lab 4 - Critical Directive.cpp
@@ -27,3 +27,33 @@ int main()
     printf("The global sum throughout the team is %lld \n", sum);
     return 0;
 }
+
+// #include<omp.h>
+// #include<iostream>
+// #define N 10000 
+
+// int main()
+// {
+//     int i;
+//     long long w, a[N], sum = 0; // Use long long to avoid overflow
+
+//     for (i = 0; i < N; i++) {
+//         a[i] = i + 1;
+//     }
+
+// #pragma omp parallel for private(w) schedule(static, 1)
+//     for (i = 0; i < N; i++)
+//     {
+//         w = (long long) i*i;  
+//         long long temp = w * a[i]; 
+// #pragma omp critical
+//         {
+//             sum += temp;  
+//         }
+//     }
+
+//     printf("The global sum throughout the team is %lld \n", sum);
+// 	return 0;
+// }
+
+
