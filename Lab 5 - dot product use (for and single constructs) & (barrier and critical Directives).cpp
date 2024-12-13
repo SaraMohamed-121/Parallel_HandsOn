52b3d781b0a8c900100e537f2098360341b374b9
Sara Mohamed <sara.sae1212@gmail.com>
2024-12-13 23:48:50 +0200

    t12

--- Lab 5 - dot product use (for and single constructs) & (barrier and critical Directives).cpp
+++ Lab 5 - dot product use (for and single constructs) & (barrier and critical Directives).cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 #include <omp.h>
+#define N 10
 using namespace std;
 int main() {
-    int size = 10, C = 0, final_sum = 0;
-    int* A = new int[size];
-    int* B = new int[size];
+    int  C = 0, final_sum = 0, A[10], B[10];
 
 #pragma omp parallel shared(A, B, final_sum) firstprivate(C)
     {
 #pragma omp for
-        for (int i = 0; i < size; i++) {
-            A[i] = B[i] = i; 
+        for (int i = 0; i < N; i++) {
+            A[i] = B[i] = i;
         }
 
-#pragma omp barrier // Ensure all threads finish initialization before proceeding
+#pragma omp barrier 
 #pragma omp single
-        cout << "Arrays are filled!!" << std::endl;
+        cout << "Arrays are filled!!" << endl;
 
 #pragma omp for
-        for (int i = 0; i < size; i++) {
-            C += A[i] * B[i]; 
+        for (int i = 0; i < N; i++) {
+            C += A[i] * B[i];
         }
 
 #pragma omp critical
-        final_sum += C; 
+        final_sum += C;
 
-#pragma omp barrier // Ensure all threads finish computation before printing the result
+#pragma omp barrier 
 #pragma omp single
-        cout << "Result = " << final_sum << std::endl;
+        cout << "Result = " << final_sum << endl;
     }
     return 0;
 }
